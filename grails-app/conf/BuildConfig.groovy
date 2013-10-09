ba0c776dca441cf54f1a65cd8fdcc152519f01d9
Kevin Smith <krsmes@gmail.com>
2013-10-09 18:26:34 -0400

    use mysql datasource for production (jdbc/krsmes-test)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -59,5 +59,8 @@ grails.project.dependency.resolution = {
         runtime ":database-migration:1.3.2"
 
         compile ':cache:1.0.1'
+
+
+        runtime 'mysql:mysql-connector-java:5.1.24'
     }
 }
--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -26,18 +26,11 @@ environments {
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
+            dialect = 'org.hibernate.dialect.MySQL5InnoDBDialect'
+            jndiName = 'java:comp/env/jdbc/krsmes-test'
+
+//            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+            pooled = false
         }
     }
 }
